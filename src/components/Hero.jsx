c6d5f111b0bcc8d2150dc354784182d40a0bebbc
lalith kumar <lalithdev123@gmail.com>
2025-03-15 15:07:17 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
    
     const timer = setTimeout(() => {
       // Animation for the text content
-      
+
       gsap.from(textRef.current, {
         opacity: 0,
         y: 50,
@@ -59,12 +59,11 @@ const Hero = () => {
             Welcome to my professional space where law converges with innovation. I’m Shail Bala Tripathy—a lawyer reimagined as a legal technologist and designer, committed to transforming the legal landscape through cutting-edge technology.
           </p>
           <div className="w-full flex place-content-center lg:place-content-start">
-            {/* <a href="#about" className="inline-block mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700 transition duration-300">Discover Now</a> */}
-         
-          <button class="bg-blue-400 mt-5 cursor-pointer text-white border border-blue-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+            
+          <a  href="#who-am-i" class="bg-blue-400 mt-5 cursor-pointer text-white border border-blue-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group" >
             <span class="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
             Discover Now
-          </button>
+          </a>
 
           </div>
         </div>
